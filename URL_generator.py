112e0f9a0cd330c1efb60c7fed41324b9c55a1ec
Michael Tobin <michael.tobin@nationalarchives.gov.uk>
2020-09-10 15:31:04 +0100

    added basic URLs

--- URL_generator.py
+++ URL_generator.py
@@ -7,7 +7,7 @@ import os
 
 apt_updates = "sudo apt-get update; sudo apt update; sudo apt-get upgrade; sudo apt upgrade"
 initialise = "cd ~; cd browsertrix; sudo git pull; sudo ./install-browsers.sh; sudo docker-compose build; sudo docker-compose up -d; cd ~; cd coronavirus.data.gov.uk_URL_generator"
-os.system(apt_updates + "; "+ initialise)
+os.system(apt_updates + "; " + initialise)
 
 home = os.path.expanduser("~")
 
