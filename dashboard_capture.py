3d8f5d2771f0c671c0c96a3f9cf745203aaafcfe
Michael Tobin <michael.tobin@nationalarchives.gov.uk>
2020-09-25 12:42:27 +0100

    only checks errors of run urls

--- dashboard_capture.py
+++ dashboard_capture.py
@@ -128,9 +128,9 @@ os.chdir(home)
 today = datetime.today().strftime('%Y%m%d%H%M%S')
 types = ["overview", "nation", "region", "nhsRegion", "utla", "ltla"]
 
-crawl_name = input("What is the name of this crawl? (Do not any of these characters (.-:) >") + "_" + today
-while ("." in crawl_name or "-" in crawl_name) or ":" in crawl_name:
-    crawl_name = input("Crawl name must not include dashes, full stops or colons. Please re-enter: > ") + "_" + today
+crawl_name = input("What is the name of this crawl? >") + "_" + today
+while any(punctuation in crawl_name for punctuation in [".", "-", "/", ":", ",", "?", "!", ";", "(", ")", "[", "]", "{", "}"]):
+    crawl_name = input("Crawl name must not punctuation (except underscore _). Please re-enter: >") + "_" + today
 
 collection_loc = f"{home}browsertrix/webarchive/collections/{crawl_name}/"
 CVDB_folder = home + "covid_dashboard/"
