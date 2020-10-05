ee665a52cc8aaf5bc84b44b4775443137a0e8ee1
Michael Tobin <michael.tobin@nationalarchives.gov.uk>
2020-10-05 10:40:46 +0100

    added troubleshoot.py

--- troubleshoot.py
+++ troubleshoot.py
@@ -4,6 +4,9 @@ import os
 home = os.path.expanduser("~") + "/"
 folder = home + "troubleshoot/"
 
+if os.path.isdir(folder) == False:
+    os.mkdir(folder)
+
 yaml = ukgwa_BX.create_yaml(["https://www.local.gov.uk", "https://www.nhs.uk"], folder, default=True)
 
 ukgwa_BX.run_BX(yaml)
