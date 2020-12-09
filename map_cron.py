8e31b0872011ea177bd958e87c5c8e460371f551
Michael Tobin <michael.tobin@nationalarchives.gov.uk>
2020-12-09 17:04:38 +0000

    automato

--- map_cron.py
+++ map_cron.py
@@ -2,7 +2,7 @@ import os, sys, time
 from datetime import datetime
 
 ROOT = os.path.dirname(os.path.abspath(__file__)) + "/"
-home = "home/mich/"
+home = "/home/mich/"
 sys.path.insert(1, f"{home}BX_tools")
 today = datetime.today().strftime("%d%m%Y")
 
