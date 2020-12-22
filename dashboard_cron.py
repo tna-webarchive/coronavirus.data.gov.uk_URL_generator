6bac6b2a9a3ab1e76ba5d0d1e76ea968e1524b5f
Michael Tobin <michael.tobin@nationalarchives.gov.uk>
2020-12-22 17:28:24 +0000

    restructured cron

--- dashboard_cron.py
+++ dashboard_cron.py
@@ -139,7 +139,7 @@ with open(f"current_areaNames_{todaystr}.csv", "r") as areaNames:
     areaNames = list(reader)
 
 both_sets = get_all_urls(areaNames)
-map_urls = list(set(both_sets[1][:100]))
+map_urls = list(set(both_sets[1]))
 
 print(today, "Dashboard capture initiated")
 capture_name = today.strftime("%b%d")#capture_cron.check_validity(input("Please enter name of capture.>"))
@@ -165,7 +165,7 @@ covid3_patch = rud.get_urls('403,429,500')
 
 os.chdir(CVDB_folder)
 
-capture_cron.capture(both_sets[0][:100]+covid3_patch, capture_name=capture_name, area=CVDB_folder, crawl_depth=1, browser="chrome:84", warc_name="dashboard_combined", progress=False, patch="y", patch_codes="403,429,500")
+capture_cron.capture(both_sets[0]+covid3_patch, capture_name=capture_name, area=CVDB_folder, crawl_depth=1, browser="chrome:84", warc_name="dashboard_combined", progress=False, patch="y", patch_codes="403,429,500")
 
 capture_folder = capture_name + "_" + today.strftime("%d%m%Y")
 
